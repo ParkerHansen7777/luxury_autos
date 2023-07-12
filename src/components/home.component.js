723df9d41bb81ab7655cfe5cdf4730e3038cbe27
ParkerHansen7777 <parkerhansen7777@gmail.com>
2023-07-12 15:26:40 -0700

    working version prior to UI fix testing

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,6 +1,5 @@
 import './page.css';
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
 import CarsList from './cars.component';
 //import NewCar from './new_car.component';
 
